66b281970d57d4bb0560da91f8fbb81197b15304
jf <jpf0628@gmail.com>
2021-11-16 19:32:46 -0800

    upated hw

---
+++ W4D1/mancala/lib/board.rb
@@ -0,0 +1,44 @@
+class Board
+  attr_accessor :cups
+  attr_reader :name1, :name2
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    self.place_stones
+  end
+
+  def place_stones
+    @cups.each_with_index do |cup, i| 
+      4.times { cup << :stone if (i != 6 && i != 13) }
+    end
+  end
+
+
+  def valid_move?(start_pos)
+    raise 'Invalid starting cup' if !(0..13).include?(start_pos)
+    raise "Starting cup is empty" if cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+  end
+
+  def winner
+  end
+end
