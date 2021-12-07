b99c8be28a73fc38286a53e7395d9f8d44e04fc5
jf <jpf0628@gmail.com>
2021-12-06 23:08:43 -0800

    helpers/layouts/partials/

--- W7D1/auth_hw/app/mailers/application_mailer.rb
+++ W7D1/auth_hw/app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'CATS@example.com'
   layout 'mailer'
+
+  def welcome_emal(user)
+    @user = user
+    @url = 'http://localhost:3000/session/new'
+    mail(to: @user.username, subject: "WELCOME!")
+  end
 end
